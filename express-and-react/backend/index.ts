8825e8a8c97d6925201735cba73833bf8a7700d2
kento <kento@toriwatari.bird>
2022-04-28 20:43:48 +0900

    ✨ Update

--- express-and-react/backend/index.ts
+++ express-and-react/backend/index.ts
@@ -1,12 +1,16 @@
 import express from "express"
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3001
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/greeting', (req, res) => {
+  res.send('Greeting!')
+})
+
 app.listen(port, () => {
-  console.log(`listening on *:${port}`);
+  console.log(`listening on *:${port}`)
 })
